06f25b528bce57fcd7c7e4df4fccd11e0da062d3
adithyaharun <adithyaharun@outlook.com>
2025-08-01 16:48:06 +0800

    chore(deps): remove utwm since it doesnt make sense

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import utwm from "unplugin-tailwindcss-mangle/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    utwm({
-      classGenerator: {
-        customGenerate: () => Math.random().toString(36).substring(2, 10),
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
