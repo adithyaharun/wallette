cdc2a70562d5d7155e25baac7861089f693aa556
adithyaharun <adithyaharun@outlook.com>
2025-08-08 04:00:44 +0800

    fix: optimize builds

--- vite.config.ts
+++ vite.config.ts
@@ -74,6 +74,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         chunkFileNames: "assets/[hash:16].js",
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
       },
     },
     chunkSizeWarningLimit: 600,
