63c98c269aae30ccbd538bf43538b99da2b28be8
adithyaharun <adithyaharun@outlook.com>
2025-08-12 23:48:01 +0800

    chore: set default vite server host to 0.0.0.0

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,85 +2,93 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), 
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
-              },   
-            }
-          }
-        ]
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        "name": "Wallette",
-        "short_name": "Wallette",
-        "icons": [
-          {
-            "src": "/pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "/pwa-maskable-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-          },
-          {
-            "src": "/pwa-maskable-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
-        ],
-        "start_url": "/",
-        "display": "standalone",
-        "background_color": "#fafafa",
-        "theme_color": "#fafafa",
-        "description": "An offline-first personal finance management app that helps you track your finance effortlessly."
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: "assets/[hash:16].js",
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      },
-    },
-    chunkSizeWarningLimit: 600,
-  }
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+				runtimeCaching: [
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: "CacheFirst",
+						options: {
+							cacheName: "google-fonts-cache",
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+							},
+						},
+					},
+				],
+			},
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+			manifest: {
+				name: "Wallette",
+				short_name: "Wallette",
+				icons: [
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-maskable-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "/pwa-maskable-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+				start_url: "/",
+				display: "standalone",
+				background_color: "#fafafa",
+				theme_color: "#fafafa",
+				description:
+					"An offline-first personal finance management app that helps you track your finance effortlessly.",
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		host: process.env.HOST ?? "0.0.0.0",
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				chunkFileNames: "assets/[hash:16].js",
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id
+							.toString()
+							.split("node_modules/")[1]
+							.split("/")[0]
+							.toString();
+					}
+				},
+			},
+		},
+		chunkSizeWarningLimit: 600,
+	},
 });
